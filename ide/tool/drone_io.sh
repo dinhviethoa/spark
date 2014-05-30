ba7235982fcf76f83748831eb6f0328018fa30ba
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2014-05-29 17:23:18 -0700

    verbose

--- ide/tool/drone_io.sh
+++ ide/tool/drone_io.sh
@@ -1,7 +1,7 @@
 pub get
 
 if [ "$DRONE" = "true" ]; then
-  dart tool/test_runner.dart --dartium --appPath=0.12.2143_0
+  dart tool/test_runner.dart --dartium --appPath=0.12.2143_0 --verbose
 else
-  dart tool/test_runner.dart --browserPath=/Applications/dart/chromium/Chromium.app/Contents/MacOS/Chromium --appPath=0.12.2143_0
+  dart tool/test_runner.dart --browserPath=/Applications/dart/chromium/Chromium.app/Contents/MacOS/Chromium --appPath=0.12.2143_0 --verbose
 fi
