c45e1339ee605f8bf44a0349df8c330f935e72e8
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2014-05-29 19:38:48 -0700

    fixed test

--- ide/tool/drone_io.sh
+++ ide/tool/drone_io.sh
@@ -6,7 +6,7 @@ fi
 pub get
 
 if [ "$DRONE" = "true" ]; then
-  dart tool/test_runner.dart --dartium --appPath=0.12.2143_0 --verbose
+  dart tool/test_runner.dart --dartium --appPath=0.12.2143 --verbose
 else
-  dart tool/test_runner.dart --browserPath=/Applications/dart/chromium/Chromium.app/Contents/MacOS/Chromium --appPath=0.12.2143_0 --verbose
+  dart tool/test_runner.dart --browserPath=/Applications/dart/chromium/Chromium.app/Contents/MacOS/Chromium --appPath=0.12.2143 --verbose
 fi
