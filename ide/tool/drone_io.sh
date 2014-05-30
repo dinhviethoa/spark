e50d13a6946098ddb927bf65c836b0fdbf372f4e
Hoa V. DINH <dinh.viet.hoa@gmail.com>
2014-05-29 20:29:16 -0700

    show version

--- ide/tool/drone_io.sh
+++ ide/tool/drone_io.sh
@@ -10,6 +10,7 @@ pub get
 
 if [ "$DRONE" = "true" ]; then
 #  dart tool/test_runner.dart --dartium --appPath=0.12.2143 --verbose
+  /usr/bin/google-chrome-unstable --version
   dart tool/test_runner.dart --chrome-dev --appPath=0.12.2143 --verbose
 else
   dart tool/test_runner.dart --browserPath=/Applications/dart/chromium/Chromium.app/Contents/MacOS/Chromium --appPath=0.12.2143 --verbose
